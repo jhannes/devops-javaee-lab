99d470c10a7b815b522186f25b4b41a4607b82a6
Johannes Brodwall <johannes@brodwall.com>
2023-06-01 23:34:06 +0200

    Prepare for demo

--- src/main/frontend/application.tsx
+++ src/main/frontend/application.tsx
@@ -85,7 +85,7 @@ function CreateTodoItem() {
 export function Application() {
   return (
     <>
-      <h1>Completed Todo Application</h1>
+      <h1>Todo Application</h1>
       <ListTodoItems />
 
       <h1>Make new todo</h1>
